608c83dc88f901830cab14937839b30da9c35d07
poreddymounika <monikaporeddy@gmail.com>
2024-10-03 17:14:34 +1000

    Project 3 commit 1

---
+++ src/p1/Sample1.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sample 1: First Line");
+
+	}
+
+}
